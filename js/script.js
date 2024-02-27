cf1483d3f131dcace9796a58ce7cd4b2294e90a4
stefanelli990 <stefan.mohenski@gmail.com>
2024-02-27 16:32:35 +0100

    fixing responsive issues, changed headline text and add wavy vector

--- js/script.js
+++ js/script.js
@@ -92,24 +92,5 @@ let year = d.getFullYear()
 
 copyrightYear.innerText = year
 
-// swiper
 
-const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-        delay: 2000,
-    },
-    breakpoints: {
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 30
-        },
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        }
-      }
-  });
   
